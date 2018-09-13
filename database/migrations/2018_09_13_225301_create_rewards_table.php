158978e8ec2f91eabd1738bbc7f49171dc9b23fa
Riku <shisoume105@gmail.com>
2018-09-13 23:09:36 +0900

    [add]rewards table

---
+++ database/migrations/2018_09_13_225301_create_rewards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRewardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rewards', function (Blueprint $table) {
+            $table->increments('reward_id');
+            $table->integer('user_id')->unsigned()->index;
+            $table->string('reward_name', 50);
+            $table->integer('rarity')->default(1);
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rewards');
+    }
+}

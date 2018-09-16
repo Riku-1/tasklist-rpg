5022f0e168d77f050bf9fef5bfc67a739ba7b8c5
Riku <shisoume105@gmail.com>
2018-09-16 13:40:33 +0900

    [change]セレクトボックスをhtml生成ではなくブレイド記法からの生成に変更

--- resources/views/reward_categories/create.blade.php
+++ resources/views/reward_categories/create.blade.php
@@ -6,19 +6,10 @@
     {!! Form::label('reward_name', '報酬の名前') !!}
     {!! Form::text('reward_name', old('reward_name'), ['class' => 'form-control']) !!}
 
-    <div class="form-row align-items-center">
-      <div class="col-auto my-1">
-        <label class="my-1 mr-2" for="rarity_select">レアリティ</label>
-        <select class="custom-select my-1 mr-sm-2" id="rarity_select" name="rarity">
-          <option value="0" selected>C</option>
-          <option value="1">B</option>
-          <option value="2">A</option>
-          <option value="3">S</option>
-        </select>
-      </div>
-    </div>
+    {!! Form::label('rarity', 'レアリティ') !!}
+    {!! Form::select('rarity', ["C", "B", "A", "S"], old('rarity')) !!}
 
     {!! Form::submit('報酬生成') !!}
-    {!! Form::close() !!}
+  {!! Form::close() !!}
   </div>
 @endsection

cc855fa0f271a861d477434bc3415e5732997eb2
Riku <shisoume105@gmail.com>
2018-09-19 00:47:50 +0900

    [change]qustsテーブルからreward_idカラムを削除
    questsとrewardsは一対多じゃなく多対多だったため削除

--- database/migrations/2018_09_19_001123_create_quests_table.php
+++ database/migrations/2018_09_19_001123_create_quests_table.php
@@ -16,14 +16,11 @@ class CreateQuestsTable extends Migration
         Schema::create('quests', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index;
-            //報酬設定をしないときのためにnullを許可
-            $table->integer('reward_id')->unsigned()->nullable()->index;
             $table->string('quest_name', 100);
             $table->integer('level')->unsigned();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('reward_id')->references('id')->on('rewards');
         });
     }
 

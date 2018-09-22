c75248a53daa6ba0dbadfca4307c91591a48f11c
Riku <shisoume105@gmail.com>
2018-09-22 11:22:45 +0900

    [add]bossテーブルを追加

--- database/migrations/2018_09_21_210113_create_lesser_enemies_table.php
+++ database/migrations/2018_09_21_210113_create_lesser_enemies_table.php
@@ -14,18 +14,21 @@ class CreateLesserEnemiesTable extends Migration
     public function up()
     {
         Schema::create('lesser_enemies', function (Blueprint $table) {
+          //ザコ敵テーブル。ボスと違ってクエストに対して一対多の関係
             $table->increments('id');
             $table->timestamps();
             $table->integer('quest_id')->unsigned()->index;
             $table->string('enemy_name', 100);
             $table->string('enemy_overview')->nullable();
             //敵のレベル（タスクの難易度）を表す。難易度を可視化してあまり難しいタスクはさらに分割させることが狙い
-            $table->integer('level');
+            //レベルによって敵画像を変更する
+            $table->integer('level')->unsigned();
             //敵のHP（タスクの進行度）を表す。
             $table->integer('hp')->default(100);
             //nullを許可。デフォルト（null）報酬をガチャ券にするため
             $table->integer('reward_id')->unsigned()->nullable()->index;
-            $table->integer('order');
+            //敵の並びを管理
+            $table->integer('order')->unsigned();
             $table->boolean('is_alive')->default(true);
 
             $table->foreign('quest_id')->references('id')->on('quests');

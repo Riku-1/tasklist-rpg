88983875d82ab5491dab2d7e6b678cd196fc3286
Riku <shisoume105@gmail.com>
2018-09-22 11:09:17 +0900

    [add]lesser_enemiesテーブルを作成
    ザコ敵を管理

---
+++ database/migrations/2018_09_21_210113_create_lesser_enemies_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLesserEnemiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lesser_enemies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('quest_id')->unsigned()->index;
+            $table->string('enemy_name', 100);
+            $table->string('enemy_overview')->nullable();
+            //敵のレベル（タスクの難易度）を表す。難易度を可視化してあまり難しいタスクはさらに分割させることが狙い
+            $table->integer('level');
+            //敵のHP（タスクの進行度）を表す。
+            $table->integer('hp')->default(100);
+            //nullを許可。デフォルト（null）報酬をガチャ券にするため
+            $table->integer('reward_id')->unsigned()->nullable()->index;
+            $table->integer('order');
+            $table->boolean('is_alive')->default(true);
+
+            $table->foreign('quest_id')->references('id')->on('quests');
+            $table->foreign('reward_id')->references('id')->on('rewards');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lesser_enemies');
+    }
+}

396fcb4a5210704a8c2da2a92e56ef531fd161fd
Riku <shisoume105@gmail.com>
2018-10-13 01:56:02 +0900

    [add]navbarにクエスト一覧と詳細ページへのリンクを追加

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,6 +13,18 @@
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav navbar-right">
           @if (Auth::check())
+            <li>{!! link_to_route('quests.index', 'クエスト一覧') !!}</li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">各クエスト<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                @php
+                  $user = Auth::user();
+                @endphp
+                @foreach ($user->quests as $quest)
+                  <li>{{ link_to_route('quests.show', $quest->quest_name, ['id' => $quest->id]) }}</li>
+                @endforeach
+              </ul>
+            </li>
             <li>{!! link_to_route('gacha.main', 'ガチャ') !!}</li>
             <li>{!! link_to_route('owned_items.index', '所持アイテム') !!}</li>
             <li class="dropdown">
